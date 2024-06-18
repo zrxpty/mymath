8412878016206f146885dcf10dcc9d24d9b06f25
qwe <streamnik666nik@gmail.com>
2024-06-18 19:21:21 +0700

    feat: qwe

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gitlab.com/zrxpty/mymath
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,8 @@
+package mymath
+
+import "math"
+
+// Sqrt - функция для вычисления квадратного корня из числа
+func Sqrt(x float64) int {
+	return int(math.Sqrt(x))
+}
